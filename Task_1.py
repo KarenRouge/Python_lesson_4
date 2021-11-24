e5a0e372335f0fc992ec6d11c55fb875c9f0e16e
KarenRouge <karenrouge1991@gmail.com>
2021-11-23 20:07:59 -0500

    Task_1 complete

--- Task_1.py
+++ Task_1.py
@@ -1,8 +1,10 @@
 from sys import argv
 
-Name, Family_name, Hours = argv
+script, Name, Family_name, Hours = argv
+
+print(argv)
 print("Имя: ", Name)
 print("Фамилия: ", Family_name)
 print("Отработанные часы: ", Hours)
-salary = (Hours * 1500) + 150000
+salary = (int(Hours) * 1500) + 150000
 print("Зарплата за текущий месяц: ", salary)
